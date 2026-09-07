06675082f8aa1523707389d7ee853613436c2ef6
sethjernigan-create <seth.jernigan@estemschools.org>
2026-09-07 13:54:50 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+/* Hall Capture offline cache.
+   When Seth sends a new version of the tool, change CACHE to the next number
+   (hall-v2, hall-v3) so phones pick up the new file instead of the old one. */
+var CACHE = "hall-v2";
+var FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES); }).catch(function(){}));
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+  }).then(function(){ return self.clients.claim(); }));
+});
+
+self.addEventListener("fetch", function(e){
+  if(e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(function(hit){
+      if(hit) return hit;
+      return fetch(e.request).then(function(res){
+        var copy = res.clone();
+        caches.open(CACHE).then(function(c){ c.put(e.request, copy); }).catch(function(){});
+        return res;
+      }).catch(function(){ return caches.match("./index.html"); });
+    })
+  );
+});
